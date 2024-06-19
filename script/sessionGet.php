c66f724e4172cb593bad0eedd6366576d2ad0952
Alessio Lavore <61512398+NotRambi@users.noreply.github.com>
2024-06-19 18:35:40 +0200

    Delete script/sessionGet.php

--- script/sessionGet.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['lang'])) {
-        echo json_encode(['status' => 'success', 'lang' => $_SESSION['lang']]);
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'Language not set']);
-    }
-?>
