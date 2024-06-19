b903266da87f7d941e5af565a721864854822564
Alessio Lavore <61512398+NotRambi@users.noreply.github.com>
2024-06-19 18:35:45 +0200

    Delete script/sessionSet.php

--- script/sessionSet.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if (isset($_POST['lang'])) {
-        $_SESSION['lang'] = $_POST['lang'];
-        echo json_encode(['status' => 'success', 'message' => 'Language set successfully']);
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'No language provided']);
-    }
-?>
